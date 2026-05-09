f5f9dc0a8b1257985ad21ba7784e5f26844a5ebb
MaxRei-dev <max@gethome.at>
2026-05-10 01:43:34 +0200

    Update WeatherSensorCfg.h

--- components/bresser_weather/WeatherSensorCfg.h
+++ components/bresser_weather/WeatherSensorCfg.h
@@ -1,41 +1,9 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-// WeatherSensorCfg.h
+// WeatherSensorCfg.h  –  MODIFIED FOR POOL SENSOR SUPPORT
 //
-// Bresser 5-in-1/6-in-1/7-in-1 868 MHz Weather Sensor Radio Receiver
-// based on CC1101, SX1276/RFM95W, SX1262 or LR1121 and ESP32/ESP8266
-//
-// https://github.com/matthias-bs/BresserWeatherSensorReceiver
-//
-// NOTE: Application/hardware specific configurations should be made in this file!
-//
-// created: 05/2022
-//
-//
-// MIT License
-//
-// Copyright (c) 2025 Matthias Prinke
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
-// This version lives here to override the one in the library.
-// It is copied to the library folder during build by pre_build.py
-//
+// Änderung gegenüber Original:
+//   MAX_SENSORS_DEFAULT  1  →  2    (Wetterstation + Pool-Thermometer)
+//   SENSOR_IDS_EXC            prüfen ob 0x792882A2 zufällig das Pool-Thermometer ist!
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #if !defined(WEATHER_SENSOR_CFG_H)
@@ -46,15 +14,18 @@
 // ------------------------------------------------------------------------------------------------
 // --- Weather Sensors ---
 // ------------------------------------------------------------------------------------------------
-#define MAX_SENSORS_DEFAULT 2       // Maximum number of sensors to be received
+
+// *** GEÄNDERT von 1 auf 2: ein Slot für Wetterstation, ein Slot für Pool-Thermometer ***
+#define MAX_SENSORS_DEFAULT 2
 
 // List of sensor IDs to be excluded - can be empty
-//#define SENSOR_IDS_EXC { 0x792882A2 }
+// ACHTUNG: Prüfe ob 0x792882A2 dein Pool-Thermometer ist!
+// Die ID steht im Log beim ersten Empfang, oder auf dem Sensorgehäuse.
+// Falls es das Pool-Thermometer ist → aus der Liste entfernen.
 #define SENSOR_IDS_EXC { 0x792882A2 }
 
 // List of sensor IDs to be included - if empty, handle all available sensors
 #define SENSOR_IDS_INC { }
-//#define SENSOR_IDS_INC { 0x83750871 }
 
 // Maximum number of sensor IDs in include/exclude list
 #define MAX_SENSOR_IDS 12
@@ -64,7 +35,7 @@
 #define WIND_DATA_FIXEDPOINT
 
 // Select appropriate sensor message format(s)
-// Comment out unused decoders to save operation time/power
+// BRESSER_6_IN_1 ist zwingend erforderlich für das Pool-Thermometer PN 7000073!
 #define BRESSER_5_IN_1
 #define BRESSER_6_IN_1
 #define BRESSER_7_IN_1
@@ -78,14 +49,9 @@
 
 #if !defined(INSIDE_UNITTEST)
     #if defined(ESP32)
-        // Option: Comment out to save data in RTC RAM
-        // N.B.:
-        // ESP8266 has RTC RAM, too, but access is different from ESP32
-        // and currently not implemented here
         #define RAINGAUGE_USE_PREFS
         #define LIGHTNING_USE_PREFS
     #else
-        // Using Preferences is mandatory on other architectures (e.g. RP2040)
         #define RAINGAUGE_USE_PREFS
         #define LIGHTNING_USE_PREFS
     #endif
@@ -107,22 +73,6 @@
 // ------------------------------------------------------------------------------------------------
 // --- Debug Logging Output ---
 // ------------------------------------------------------------------------------------------------
-// - ESP32:
-//   CORE_DEBUG_LEVEL is set in Adruino IDE:
-//   Tools->Core Debug Level: "<None>|<Error>|<Warning>|<Info>|<Debug>|<Verbose>"
-//   https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal-log.h
-//
-// - ESP8266:
-//   DEBUG_ESP_PORT is set in Arduino IDE:
-//   Tools->Debug port: "<None>|<Serial>|<Serial1>"
-//
-// - RP2040:
-//   DEBUG_RP2040_PORT is set in Arduino IDE:
-//   Tools->Debug port: "<Disabled>|<Serial>|<Serial1>|<Serial2>"
-//
-//   Replacement for
-//   https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal-log.h
-//   on ESP8266 and RP2040:
 #if defined(ESP8266) || defined(ARDUINO_ARCH_RP2040)
     #define ARDUHAL_LOG_LEVEL_NONE      0
     #define ARDUHAL_LOG_LEVEL_ERROR     1
@@ -136,8 +86,7 @@
     #elif defined(DEBUG_ESP_PORT)
         #define DEBUG_PORT DEBUG_ESP_PORT
     #endif
-    
-    // Set desired level here if not defined elsewhere!
+
     #if !defined(CORE_DEBUG_LEVEL)
         #define CORE_DEBUG_LEVEL ARDUHAL_LOG_LEVEL_INFO
     #endif
@@ -167,7 +116,6 @@
      #else
         #define log_v(...) {}
      #endif
-
 #endif
 
 #define STR_HELPER(x) #x
